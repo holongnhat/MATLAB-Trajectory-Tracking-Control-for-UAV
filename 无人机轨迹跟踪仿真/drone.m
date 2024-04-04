45404aa1d3f56d62d3d7bd9c587f76e05f3da1cc
Ho Long Nhat <nhatpro@gmail.com>
2024-04-04 11:59:08 +0700

    Updated

--- 无人机轨迹跟踪仿真/drone.m
+++ 无人机轨迹跟踪仿真/drone.m
@@ -6,9 +6,18 @@ Cbn = [cos(psai)*cos(theta),cos(psai)*sin(theta)*sin(phi)-sin(psai)*cos(phi),...
     -sin(theta),cos(theta)*sin(phi),cos(theta)*cos(phi)
     ];
 
+% Chieu dai 2 truc cua Drone
+% ========================================================
 x0 = 0; y0 = 0; z0 = 0;
-point1 = [x0-1,y0,z0]; point2 = [x0+1,y0,z0];
-point3 = [x0,y0-1,z0]; point4 = [x0,y0+1,z0];
-point1_trans = Cbn*point1';point2_trans = Cbn*point2';
-point3_trans = Cbn*point3';point4_trans = Cbn*point4';
+x = 0.5;
+point1 = [x0-x,y0,z0]; 
+point2 = [x0+x,y0,z0];
+point3 = [x0,y0-x,z0]; 
+point4 = [x0,y0+x,z0];
+% ========================================================
+
+point1_trans = Cbn*point1';
+point2_trans = Cbn*point2';
+point3_trans = Cbn*point3';
+point4_trans = Cbn*point4';
 end
